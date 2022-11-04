2fa5b8339b090f5038ac06f120d791e6cf410c6b
gaurovgiri <gaurovgiri@gmail.com>
2022-11-04 14:38:21 +0545

    Fix traversing broken issue

--- src/alert.c
+++ src/alert.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ncurses.h>
 #include <malloc.h>
+#include <limits.h>
 
 
 
@@ -12,8 +13,8 @@ void alert(const char *file)
     getmaxyx(stdscr, maxy, maxx);
     WINDOW *alertScreen = newwin(ALERT_ROW, ALERT_COL, maxy / 4, maxx / 4);
     box(alertScreen,0,0);
-    char *alertText = (char *)malloc(sizeof(char)*strlen(file));
-    sprintf(alertText,"%s is a file",file);
+    char alertText[PATH_MAX];
+    snprintf(alertText,sizeof(alertText),"%s is a file",file);
     wcenter(alertScreen, ALERT_ROW / 2, alertText);
     getch();
     delwin(alertScreen);
